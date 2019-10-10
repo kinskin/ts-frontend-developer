2a2a110214eaecdb5320aa4b097b8d9dbe0512e0
kinskin <md.asshikin@gmail.com>
2019-10-10 23:57:33 +0800

    for solution_c, the method of summing in using while loop

--- src/three_ways_to_sum_to_n/solution_c.js
+++ src/three_ways_to_sum_to_n/solution_c.js
@@ -4,5 +4,13 @@
 // Output: `return` - summation to `n`, i.e. sum_to_n(5) === 1 + 2 + 3 + 4 + 5 === 15.
 
 var sum_to_n = function(n) {
-    // your code here
+    let sum = 0;
+    let i = 0;
+    while(i <= n){
+        sum += i;
+        i++;
+    }
+    return sum
 };
+
+console.log('this is using while loop: ', sum_to_n(5))
